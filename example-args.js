337e7894aac4b2b4a87ab0c5a1256a848d9e69d7
Jorge Gallardo <jorge8486@gmail.com>
2016-01-18 13:26:24 -0500

    added yargs to get command line arguments

---
+++ example-args.js
@@ -0,0 +1,14 @@
+var argv = require('yargs').argv;
+var command = argv._[0];
+
+console.log(argv);
+
+if(command === 'hello' && typeof argv.name !== 'undefined') {
+  if(typeof argv.lastname !== 'undefined') {
+    console.log('Hello ' + argv.name + ' ' + argv.lastname + '!');
+  } else {
+    console.log('Hello ' + argv.name + '!');
+  }
+} else if(command === 'hello') {
+  console.log('Hello world!');
+}
