11f289c1a93d318a3cfefa4eb909dbbd3baff652
Jorge Gallardo <jorge8486@gmail.com>
2016-01-22 22:51:46 -0500

    added shortcuts and descuts and help descriptions

--- example-args.js
+++ example-args.js
@@ -1,4 +1,20 @@
-var argv = require('yargs').argv;
+var argv = require('yargs')
+  .command('hello', 'Greets the user', function(yargs) {
+    yargs.options({
+      name: {
+        demand: true,
+        alias: 'n', // alias is like a shortcut
+        description: 'Your first name goes here'
+      },
+      lastname: {
+        demand: true,
+        alias: 'l',
+        description: 'Your last name goes here'
+      }
+    }).help('help');
+  })
+  .help('help') //gives the description found in the .command()
+  .argv;
 var command = argv._[0];
 
 console.log(argv);
