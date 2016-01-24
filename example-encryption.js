1b1f0866c556dda38d03a3c9129098acad4953e5
Jorge Gallardo <jorge8486@gmail.com>
2016-01-24 15:15:30 -0500

    practicing encryption

---
+++ example-encryption.js
@@ -0,0 +1,18 @@
+var crypto = require('crypto-js');
+
+var secretMessage = {
+  name: 'Jorge',
+  secretName: '007'
+};
+var secretKey = '123abc';
+
+// encrypt
+var encryptedMessage = crypto.AES.encrypt(JSON.stringify(secretMessage), secretKey);
+console.log('Encrypted Message: ' + encryptedMessage);
+
+// decrypt
+var bytes = crypto.AES.decrypt(encryptedMessage, secretKey);
+var decryptedMessage = JSON.parse(bytes.toString(crypto.enc.Utf8));
+
+console.log(decryptedMessage);
+console.log(decryptedMessage.secretName);
